003a75813b809d85ce3b74f79072ba637e8b01f1
david <dh@ptica.com>
2021-03-19 14:29:07 +0100

    playing around

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Routes from './Routes';
 
 function App() {
   return (
     <div>
       <h1>Hello from App</h1>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/profile'>Profile</Link>
-        </li>
-        <li>
-          <Link to='anotherroute'>Another</Link>
-        </li>
-      </ul>
-      <Routes />
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='anotherroute'>Another</Link>
+          </li>
+        </ul>
+        <Routes />
+      </BrowserRouter>
+
+      <div>This is Footer</div>
     </div>
   );
 }
